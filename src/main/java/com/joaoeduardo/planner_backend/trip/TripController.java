66419a0bf24be5b44fe7b12f6a37b75e629c6743
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-08-01 18:19:14 -0300

    feat: Creating Update Trip Endpoint

--- src/main/java/com/joaoeduardo/planner_backend/trip/TripController.java
+++ src/main/java/com/joaoeduardo/planner_backend/trip/TripController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -39,6 +41,27 @@ public class TripController {
         Optional<Trip> trip = tripRepository.findById(id);
 
         return trip.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id,@RequestBody TripRequestPayload tripRequestPayload){
+
+        Optional<Trip> trip = tripRepository.findById(id);
+
+        if(trip.isPresent()){
+
+            Trip rawTrip = trip.get();
+            rawTrip.setDestination(tripRequestPayload.destination());
+            rawTrip.setStartsAt(LocalDateTime.parse(tripRequestPayload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            rawTrip.setEndsAt(LocalDateTime.parse(tripRequestPayload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+
+            tripRepository.save(rawTrip);
+
+            return ResponseEntity.ok(rawTrip);
+        }
+
+        return ResponseEntity.notFound().build();
 
     }
 
