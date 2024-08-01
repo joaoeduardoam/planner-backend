1a14cd3052f6ad31e890e1f8befb082aa5321fad
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2024-07-31 21:44:17 -0300

    feat: Creating Details Trip Endpoint

--- src/main/java/com/joaoeduardo/planner_backend/trip/TripController.java
+++ src/main/java/com/joaoeduardo/planner_backend/trip/TripController.java
@@ -4,10 +4,10 @@ import com.joaoeduardo.planner_backend.participant.ParticipantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/trips")
@@ -31,6 +31,14 @@ public class TripController {
         participantService.registerParticipantsToEvent(tripRequestPayload.emails_to_invite(),newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = tripRepository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
 
     }
 
